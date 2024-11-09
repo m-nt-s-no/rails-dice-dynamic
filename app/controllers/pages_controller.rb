d8dfb953519e12d55d8efd0516610c8922467a4f
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-11-09 02:33:43 +0000

    added route, controller method, view for dynamic path

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,17 @@ class PagesController < ApplicationController
   def home
     render({:template => "pages_templates/home"})
   end
+
+  def roll
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@sides)
+      @rolls.push(die)
+    end
+
+    render({:template => "pages_templates/flexible"})
+  end
 end
