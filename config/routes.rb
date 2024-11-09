82fc64c60cf635e45e1dbbf3eb6ae7a3a10e7132
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-11-09 02:19:55 +0000

    added route, controller method, and view for home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-
+  get("/", {:controller => "pages", :action => "home"})
+  get("/dice/:number_of_dice/:how_many_sides", {:controller => "pages", :action => "roll"})
 end
